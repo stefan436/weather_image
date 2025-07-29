fa95dbad094e88ce42ac11344de5e738718b091c
stefan436 <stefan.wiedemann01@web.de>
2025-07-29 21:06:27 +0200

    Delete docs/Hamburgermenu.js

--- docs/Hamburgermenu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerIcon = document.getElementById('hamburger-icon');
-    const navMenu = document.getElementById('nav-menu');
-
-    hamburgerIcon.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-        hamburgerIcon.classList.toggle('open');
-    });
-});
